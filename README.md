4723a8d91ae776371d4f45a9f675caf64af8a7b1
Harshal2703 <harshalchaudhari2703@gmail.com>
2024-05-01 14:29:12 +0530

    ui updates

--- README.md
+++ README.md
@@ -3,10 +3,6 @@
 
 docEZ is an innovative RAG (Retrieval Augmented Generation) application that allows users to upload their documents and engage in conversations with them using advanced language models (LLMs). Leveraging vector embeddings for semantic searching, docEZ enables efficient retrieval of information from uploaded documents. The application seamlessly integrates query and context from semantic searches through LLMs to generate accurate responses.
 
-
-
-
-
 ## Download Models
 
 To get started with docEZ, you can download the necessary models from [ollama.com.](https://ollama.com/) After downloading, you can use the following commands in your terminal:
@@ -14,10 +10,10 @@ To get started with docEZ, you can download the necessary models from [ollama.co
 ```bash
   ollama pull llama3
 ```
-
 ```bash
   ollama pull mxbai-embed-large
 ```
+
 ## Installations
 To set up docEZ on your local machine, follow these installation steps:
 
@@ -28,9 +24,8 @@ To set up docEZ on your local machine, follow these installation steps:
   docEZ_env/Scripts/Activate.ps1
 ```
 ```bash
-  pip install -r requirements.txt
+  pip install -r requirements.txt`
 ```
-
 
 ## Run
 Once installed, you can run docEZ using the following command:
--- app.py
+++ app.py
@@ -3,28 +3,154 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 import ollama
 import chromadb
+import os
+from docx import Document
+import re
+import json
+import uuid
+
+
+st.title("docEZ")
+
+def getUploadedFileInfo():
+    client = chromadb.PersistentClient(
+        path="E:\Mini Projects\Mini Project (Sem 6)\docEZ\embeddings"
+    )
+    collection = client.get_collection(name="uploadedFilesInfo")
+    filesInfo = collection.get()
+    filesInfo = [json.loads(i) for i in filesInfo["documents"]]
+    return filesInfo
+
+
+def loadChat(info):
+    client = chromadb.PersistentClient(
+        path="E:\Mini Projects\Mini Project (Sem 6)\docEZ\embeddings"
+    )
+    st.session_state["currentCollection"] = info
+    st.header(f"your chats with {info['ogFileName']}")
+
+
+    collection = client.get_collection(name=f"chat_{info['fileName']}")
+    chats = collection.get()
+    chats = [json.loads(i) for i in chats["documents"]]
+    for i in chats:
+        if i["role"] == "user":
+            st.chat_message(i["role"], avatar="🧑‍💻").write(i["content"])
+        else:
+            st.chat_message(i["role"], avatar="🤖").write(i["content"])
+    
 
 
 
+if "currentCollection" not in st.session_state:
+    filesInfo = getUploadedFileInfo()
+    if filesInfo:
+        loadChat(filesInfo[0])
+    else:
+        st.session_state["currentCollection"] = None
+
+deleteChat = st.button('delete chats' , type="primary")    
+if deleteChat:
+    client = chromadb.PersistentClient(path="E:\Mini Projects\Mini Project (Sem 6)\docEZ\embeddings")
+    client.delete_collection(name=f'chat_{st.session_state["currentCollection"]["fileName"]}')
+    client.create_collection(name=f'chat_{st.session_state["currentCollection"]["fileName"]}')
+    loadChat(st.session_state["currentCollection"])
 
 
-def clearMsg():
-    st.session_state.messages = []
+query = st.chat_input("Say something")
+if query and st.session_state["currentCollection"]:
+    loadChat(info=st.session_state["currentCollection"])
+    client = chromadb.PersistentClient(
+        path="E:\Mini Projects\Mini Project (Sem 6)\docEZ\embeddings"
+    )
+    collection = client.get_collection(
+        name=st.session_state["currentCollection"]["fileName"]
+    )
+    
+    response = ollama.embeddings(prompt=query, model="mxbai-embed-large")
+    results = collection.query(query_embeddings=[response["embedding"]], n_results=3)
+    context = ""
+    for i in results["documents"][0]:
+        context += i
+    # st.write(query , st.session_state["currentCollection"] , context)
 
-st.set_page_config(page_title="docEZ")
-st.header("Ask your PDF...")
-pdf = st.file_uploader("Upload your PDF", type="pdf")
-st.button("Clear chat", type="primary" , on_click=clearMsg)
-def createChunks(pdf):
-    pdf_reader = PdfReader(pdf)
+
+    collection = client.get_collection(
+        name=f"chat_{st.session_state['currentCollection']['fileName']}"
+    )
+
+    st.chat_message("user", avatar="🧑‍💻").write(query)
+
+    data = {"role": "user", "content": query}
+    json_str = json.dumps(data)
+    collection.add(ids=[str(uuid.uuid1())], documents=[json_str])
+
+    st.session_state["full_message"] = ""
+
+    def generate_response():
+        prompt = f"'''Using this data: {context}'''. '''Respond to this prompt: {query}''' don't add anything extra from your side if you are unable to find answer of query in context just say out of context don't generate anything from your side"
+        response = ollama.chat(
+            model="llama3", stream=True, messages=[{"role": "user", "content": prompt}]
+        )
+        for partial_resp in response:
+            token = partial_resp["message"]["content"]
+            st.session_state["full_message"] += token
+            yield token
+
+    st.chat_message("assistant", avatar="🤖").write_stream(generate_response)
+    data = {"role": "assistant", "content": st.session_state["full_message"]}
+    json_str = json.dumps(data)
+    collection.add(ids=[str(uuid.uuid1())], documents=[json_str])
+
+
+    
+ 
+
+def saveUploadedFile(uploadedFile):
+    path = os.path.join(os.getcwd(), "input_files", uploadedFile.name)
+    with open(path, "wb") as f:
+        f.write(uploadedFile.getvalue())
+    return path
+
+
+def extractTextFromPdf(filePath):
     text = ""
-    for page in pdf_reader.pages:
-        text += page.extract_text()
-    text_splitter = CharacterTextSplitter(
+    with open(filePath, "rb") as pdf:
+        pdfReader = PdfReader(pdf)
+        for page in pdfReader.pages:
+            text += page.extract_text()
+    return text
+
+
+def extractTextFromDocx(filePath):
+    doc = Document(filePath)
+    text = []
+    for para in doc.paragraphs:
+        text.append(para.text)
+    return "\n".join(text)
+
+
+def extractTextFromTxt(filePath):
+    with open(filePath, "r") as f:
+        text = f.read()
+    return text
+
+
+def extractTextFromFile(filePath):
+    _, fileExtension = os.path.splitext(filePath)
+    if fileExtension == ".pdf":
+        return extractTextFromPdf(filePath)
+    elif fileExtension == ".docx":
+        return extractTextFromDocx(filePath)
+    elif fileExtension == ".txt":
+        return extractTextFromTxt(filePath)
+
+
+def createChunksFromText(text):
+    textSplitter = CharacterTextSplitter(
         separator="\n", chunk_size=1000, chunk_overlap=100, length_function=len
     )
-    chunks = text_splitter.split_text(text)
-    return chunks
+    return textSplitter.split_text(text)
 
 
 def createEmbeddingsFromChunk(collection, chunks):
@@ -34,49 +160,75 @@ def createEmbeddingsFromChunk(collection, chunks):
         collection.add(ids=[str(i)], embeddings=[embedding], documents=[d])
 
 
-
-if pdf is not None:
-    chunks = createChunks(pdf)
-    client = chromadb.Client()
-    collection = None
-    for i in client.list_collections():
-        if i.name == pdf.name:
-            collection = client.get_collection(name=pdf.name)
-    if not collection:
-        collection = client.create_collection(name=pdf.name)
-        createEmbeddingsFromChunk(collection, chunks)
-        print(f"embeddings created! : {pdf.name}")
-
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-
-    for msg in st.session_state.messages:
-        if msg["role"] == "user":
-            st.chat_message(msg["role"], avatar="🧑‍💻").write(msg["content"])
-        else:
-            st.chat_message(msg["role"], avatar="🤖").write(msg["content"])
+def validateCollectionName(collectionName):
+    collectionName = collectionName.strip()
+    collectionName = re.sub(r"[^\w\-]", "", collectionName)
+    collectionName = re.sub(r"\.{2,}", "", collectionName)
+    if collectionName.startswith(".") or collectionName.endswith("."):
+        collectionName = collectionName.replace(".", "_")
+    if len(collectionName) > 63:
+        collectionName = collectionName[:63]
+    if len(collectionName) < 3:
+        collectionName = collectionName.ljust(3, "0")
+    if not collectionName[0].isalnum():
+        collectionName = "A" + collectionName[1:]
+    if not collectionName[-1].isalnum():
+        collectionName = collectionName[:-1] + "A"
+    return collectionName
 
 
-    if query := st.chat_input():
-        response = ollama.embeddings(prompt=query, model="mxbai-embed-large")
-        results = collection.query(
-            query_embeddings=[response["embedding"]], n_results=3
-        )
-        context = ""
-        for i in results["documents"][0]:
-            context += i
+def displayStoredFiles():
+    filesInfo = getUploadedFileInfo()
+    col1, col2= st.sidebar.columns(2,gap="small")
+    for i, j in enumerate(filesInfo):
+        # if st.sidebar.button(label=j["ogFileName"], key=i):
+        #     loadChat(j)
+        if col1.button(label=j["ogFileName"], key=i):
+            loadChat(j)
+        if col2.button(f"❎", key=i+1000000):
+            client = chromadb.PersistentClient(path="E:\Mini Projects\Mini Project (Sem 6)\docEZ\embeddings")
+            client.delete_collection(name=j["fileName"])
+            client.delete_collection(name=f'chat_{j["fileName"]}')
+            collection = client.get_collection(name="uploadedFilesInfo")
+            data = collection.get()
+            filesInfo = [json.loads(i) for i in data["documents"]]
+            ids = [i for i in data["ids"]]
+            for c , file in enumerate(filesInfo):
+                if file["fileName"] == j["fileName"]:
+                    collection.delete(ids=[ids[c]])
+                    break
+            st.experimental_rerun()
+        
 
-        st.session_state.messages.append({"role": "user", "content": query})
-        st.chat_message("user", avatar="🧑‍💻").write(query)
-        st.session_state["full_message"] = ""
-        def generate_response():
-            prompt=f"'''Using this data: {context}'''. '''Respond to this prompt: {query}''' don't add anything extra from your side if you are unable to find answer of query in context just say out of context don't generate anything from your side"
-            response = ollama.chat(model='llama3', stream=True, messages=[{'role': 'user', 'content': prompt}])
-            for partial_resp in response:
-                token = partial_resp["message"]["content"]
-                st.session_state["full_message"] += token
-                yield token
-        st.chat_message("assistant", avatar="🤖").write_stream(generate_response)
-        st.session_state.messages.append({"role": "assistant", "content": st.session_state["full_message"]})
+def main():
+    client = chromadb.PersistentClient(
+        path="E:\Mini Projects\Mini Project (Sem 6)\docEZ\embeddings"
+    )
+    collectionList = [i.name for i in client.list_collections()]
+    st.sidebar.title("File Uploader")
+    uploadedFile = st.sidebar.file_uploader(
+        "Choose a file", type=["txt", "docx", "pdf"]
+    )
+    if uploadedFile is not None:
+        fileName = validateCollectionName(collectionName=uploadedFile.name)
+        if fileName not in collectionList:
+            collection = client.create_collection(name=fileName)
+            path = saveUploadedFile(uploadedFile=uploadedFile)
+            text = extractTextFromFile(filePath=path)
+            chunks = createChunksFromText(text=text)
+            createEmbeddingsFromChunk(collection, chunks)
+            collection = client.get_or_create_collection(name="uploadedFilesInfo")
+            info = {
+                "fileName": fileName,
+                "ogFileName": uploadedFile.name,
+                "pathToFile": path,
+            }
+            json_str = json.dumps(info)
+            collection.add(ids=[str(uuid.uuid1())], documents=[json_str])
+            client.create_collection(name=f"chat_{fileName}")
+
+    displayStoredFiles()
 
 
+if __name__ == "__main__":
+    main()
